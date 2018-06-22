b43f2ec42ee15845ad10a7c6abffb00b877b03e6
Shane Danner <sdanner08@yahoo.com>
2018-06-21 21:26:04 -0400

    Comments on code

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+/*
+index.html (login page)
+-need input capture 
+
+-"remember me" set to local storage to auto generate upon refresh.
+    -Should we prompt for saving to key chain/ chrome browser?
+
+-on click function to push email and pass to fire base to see if its on there or not.
+    ----https://firebase.google.com/docs/auth/web/password-auth---
+    ----https://www.youtube.com/watch?v=YWfgtsoTxwk---
+    -If yes move them to homescreen.html page
+    -If no promps them for sign up
+        -need a message to generate in div for sign up in red font
+        -email is sent? then they have to agree to terms? trying to be as realistic as possible
+        -Needs to push to a sign up page ----https://www.w3schools.com/howto/howto_css_signup_form.asp---
+     
+
+
+*/
+
+
+
+
+
+/*
+homescreen.html
+-Well we need to figure out what we are doing lol.
+-navbar has some items I left for setting up naviagtion for when we decided what we are doing.
+
+-Need a sign out fuction that pushes them back to Index.html
+*/
