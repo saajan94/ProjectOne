d703b36264e673f23bf1d836fc686aeb4d1fa992
saajan94 <saajan1994@aol.com>
2018-06-30 10:09:23 -0400

    Commit to pull

--- assets/javascript/calendar.js
+++ assets/javascript/calendar.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
     $("#calendar").fullCalendar({
+        theme: true,
+        businessHours: true,
+        editable: true,
         header: {
-            left: "month, agendaWeek, agendaDay",
-            center: "title",
+            left: 'month,agendaWeek,agendaDay',
+            center: 'title',
             right: "prevYear, prev, next, nextYear"
         },
     });
