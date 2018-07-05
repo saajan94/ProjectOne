4cfa430ee5a92235b963f3fc897b726150a53523
Shane Danner <sdanner08@yahoo.com>
2018-07-05 19:47:26 -0400

    comments and css

--- assets/javascript/displayJobs.js
+++ assets/javascript/displayJobs.js
@@ -1,6 +1,4 @@
-
-
-
+//Linking Firebase to pull jobs from
 $(function (){
 
   var config = {
@@ -14,20 +12,21 @@ $(function (){
   firebase.initializeApp(config);
   database = firebase.database();
 
+//collects the jobs from Firebase to be displayed
   database.ref('/jobs').on('child_added', function(snap){
    var card = $("<div>").attr({
       class: "card mb-4",
       style: "width: 100%"
     })
-
+    //sets attributes for the jobs pulled from FB
     var body = $("<div>").attr({
       class: "card-body"
     })
-
+    //More attributes being added
     var ul = $("<ul>").attr({
       class: "list-group"
     })
-
+    //Pulling the information from FB based on the items we need to display
     var li = $("<li>").attr("class", "list-group-item list-group-item-dark").append('<span class="font-weight-bold">Tittle: </span>'+snap.val().jobTitle)
     ul.append(li)
     var li = $("<li>").attr("class", "list-group-item").append('<span class="font-weight-bold">Description: </span>'+ snap.val().jobDescription)
@@ -39,26 +38,22 @@ $(function (){
     var li = $("<li>").attr("class", "list-group-item").append('<span class="font-weight-bold">Salary: </span>'+ snap.val().salary)
     ul.append(li)
     var title = snap.val().jobTitle
-  
+    //Adding the information how we want it to be displayed 
     var apply = $("<button>").attr({type: 'button',class: "btn btn-primary float-right mt-3",'data-toggle':'modal','data-target':'#apply', key:snap.key, title: title}).append("Apply")
     body.append(ul)
     body.append(apply)
     card.append(body)
 
 
-  
+  //Places the card on the HTML page
     $(".row").append(card)
   
-
+      //debugging
       console.log(snap.key)
-
-
-      // $(document).on("click",".apply", function(){
-      //   $("#apply").modal('show')
-      // })
 
       
       })
+      //setting up buttons for applying
       $('#apply').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget) // Button that triggered the modal
         var jobId = button.attr('key')
@@ -67,12 +62,4 @@ $(function (){
         modal.find('.modal-title').text('Apply for: ' + title)
 
   })
-
-
-
-
-
-
-
-
 })
